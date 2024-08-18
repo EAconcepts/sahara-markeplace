3e0725ed273b3eead43101d9226535e34350e152
Emmanuel <hahyordeji@gmail.com>
2024-08-18 19:34:08 +0100

    worked on vendor dashboard

--- src/app/dashboard/(components)/sidemenu.tsx
+++ src/app/dashboard/(components)/sidemenu.tsx
@@ -16,14 +16,20 @@ import { NavlinksProps } from "@/app/sellers/dashboard/(components)/navlinks";
 import { useAuth } from "@/utils/useAuth";
 import Link from "next/link";
 
-const Sidemenu = ({links, settingsLink}:{links:NavlinksProps[]; settingsLink?:string}) => {
-  const {user} = useAuth()
+const Sidemenu = ({
+  links,
+  settingsLink,
+}: {
+  links: NavlinksProps[];
+  settingsLink?: string;
+}) => {
+  const { user } = useAuth();
   const path = usePathname();
   const router = useRouter();
   return (
-    <div className="px-[24px] pt-[12px] border-r-[1px] border-border">
+    <div className="border-r-[1px] border-border px-[24px] pt-[12px]">
       {/* logo */}
-      <Link href={'/'} className="flex items-center gap-x-[4px] pl-[24px]">
+      <Link href={"/"} className="flex items-center gap-x-[4px] pl-[24px]">
         <Image
           src={logo}
           width={56}
@@ -37,14 +43,15 @@ const Sidemenu = ({links, settingsLink}:{links:NavlinksProps[]; settingsLink?:st
       </Link>
       {/* Menu */}
       <div className="mt-[32px] flex w-full flex-col items-start gap-y-[4px] border-b-[1px] border-border pb-[12px] font-openSans text-[14px] font-[400] leading-[20.3px]">
-        {links?.map((menu:any, index:number) => (
-          <Button
+        {links?.map((menu: any, index: number) => (
+         <div   key={index}>
+         <Button
             onClick={() => router.push(menu.path)}
-            key={index}
-            className={` ${path == menu.path ? "bg-greenPrimary text-white" : "bg-transparent text-blackPrimary"} flex h-[44px] w-full items-center justify-between rounded-[4px] px-[16px] py-[12px]`}
+          
+            className={`relative ${path == menu.path ? "bg-greenPrimary text-white" : "bg-transparent text-blackPrimary"} flex h-[44px] w-full items-center justify-between rounded-[4px] px-[16px] py-[12px]`}
           >
             <div className="flex items-center gap-x-[12px]">
-             {menu.icon && <menu.icon />}
+              {menu.icon && <menu.icon />}
               <span
                 className={` ${path == menu.path && "font-[600]"} text-[14px] font-[400] leading-[20.3px]`}
               >
@@ -56,7 +63,26 @@ const Sidemenu = ({links, settingsLink}:{links:NavlinksProps[]; settingsLink?:st
                 {menu.value}
               </p>
             )}
+          
           </Button>
+          {menu?.sublinks && (
+              <div className="pl-[44px] flex flex-col gap-y-[8px] ">
+                {menu?.sublinks?.map((sub: any, index: number) => (
+                  <div 
+            onClick={() => router.push(sub.path)}
+
+                  className="flex items-center gap-x-[12px]" key={index}>
+                    {sub.icon && <sub.icon />}
+                    <span
+                      className={` ${path == sub.path && "font-[600]"} text-[14px] font-[400] leading-[20.3px]`}
+                    >
+                      {sub.title}
+                    </span>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
         ))}
       </div>
 
@@ -74,7 +100,10 @@ const Sidemenu = ({links, settingsLink}:{links:NavlinksProps[]; settingsLink?:st
               Help & Support
             </span>
           </button>
-          <button onClick={()=>router.push(`${settingsLink}`)} className="flex items-center gap-x-[12px] px-[16px] py-[12px]">
+          <button
+            onClick={() => router.push(`${settingsLink}`)}
+            className="flex items-center gap-x-[12px] px-[16px] py-[12px]"
+          >
             <Settings02Icon />{" "}
             <span className="text-[14px] font-[400] leading-[20.3px] text-blackPrimary">
               Settings
@@ -83,18 +112,19 @@ const Sidemenu = ({links, settingsLink}:{links:NavlinksProps[]; settingsLink?:st
           <div className="h-[1px] w-full bg-border"></div>
         </div>
         {/* logout */}
-        <div className="w-full mt-[10px] items-center flex justify-between px-[24px] py-[32px]">
+        <div className="mt-[10px] flex w-full items-center justify-between px-[24px] py-[32px]">
           <div className="flex items-center gap-x-[12px]">
             <UserCircleIcon />
             <div className="flex flex-col">
               <h6 className="text-[14px] font-[700] leading-[20.3px] text-blackPrimary">
-               {user?.first_name} {user?.last_name}
+                {user?.first_name} {user?.last_name}
               </h6>
-              <span className="text-[14px] font-[400] leading-[20.3px] text-[#787C83]">Verify email</span>
+              <span className="text-[14px] font-[400] leading-[20.3px] text-[#787C83]">
+                Verify email
+              </span>
             </div>
           </div>
-          <Logout02Icon/>
-
+          <Logout02Icon />
         </div>
       </div>
     </div>
--- src/app/sellers/dashboard/(components)/navlinks.tsx
+++ src/app/sellers/dashboard/(components)/navlinks.tsx
@@ -1,45 +1,105 @@
-'use client'
+"use client";
 
-import { AlignBoxMiddleLeftIcon, DashboardSquare03Icon, DiscountIcon, FavouriteIcon, HugeiconsProps, Message01Icon, News01Icon, PackageIcon, ProductLoadingIcon, Settings02Icon, UserAccountIcon, UserMultiple02Icon } from "hugeicons-react";
+import {
+  AlignBoxMiddleLeftIcon,
+  DashboardSquare03Icon,
+  DiscountIcon,
+  FavouriteIcon,
+  FolderLibraryIcon,
+  HugeiconsProps,
+  Message01Icon,
+  News01Icon,
+  PackageAddIcon,
+  PackageIcon,
+  ProductLoadingIcon,
+  Settings02Icon,
+  UserAccountIcon,
+  UserMultiple02Icon,
+} from "hugeicons-react";
 import { FC, RefAttributes } from "react";
 
+export interface NavlinksProps {
+  icon: FC<Omit<HugeiconsProps, "ref"> & RefAttributes<SVGSVGElement>>;
+  title: string;
+  path: string;
+  value?: number;
+  sublinks?: any;
+}
+export const sellersNavLinks: NavlinksProps[] = [
+  {
+    icon: DashboardSquare03Icon,
+    title: "Dashboard",
+    path: "sellers/dashboard",
+  },
+  {
+    icon: ProductLoadingIcon,
+    title: "Products",
+    path: "#",
+    sublinks: [
+      { title: "Product List", path: "/sellers/dashboard/product-listings",icon: PackageIcon, },
+      { title: "Categories", path: "/sellers/dashboard/categories",icon: FolderLibraryIcon, },
+      { title: "Add Products", path: "/sellers/dashboard/add-products", icon: PackageAddIcon, },
+    ],
+  },
+  {
+    icon: PackageIcon,
+    title: "My Orders",
+    path: "/sellers/dashboard/my-orders",
+  },
+  {
+    icon: FavouriteIcon,
+    title: "Reviews",
+    path: "sellers/dashboard/Reviews",
+    value: 10,
+  },
+  { icon: DiscountIcon, title: "Quotes", path: "sellers/dashboard/quotes" },
+];
 
-export interface NavlinksProps{
-    icon: FC<Omit<HugeiconsProps, "ref"> & RefAttributes<SVGSVGElement>>;
-    title: string;
-    path: string;
-    value?: number;
-  }
-export const sellersNavLinks: NavlinksProps[] =[
-  
-    { icon: DashboardSquare03Icon, title: "Dashboard", path: "sllers/dashboard" },
-    { icon: ProductLoadingIcon, title: "Products", path: "sellers/dashboard/product-listings" },
-    { icon: PackageIcon, title: "My Orders", path: "sellers/dashboard/my-orders" },
-    { icon: FavouriteIcon, title: "Reviews", path: "sellers/dashboard/Reviews" , value: 10 },
-    { icon: DiscountIcon, title: "Quotes", path: "sellers/dashboard/quotes" },
+export const navlinks: NavlinksProps[] = [
+  { icon: DashboardSquare03Icon, title: "Dashboard", path: "/dashboard" },
+  { icon: PackageIcon, title: "My Orders", path: "/dashboard/my-orders" },
+  { icon: FavouriteIcon, title: "Wishlist", path: "/dashboard/wishlist" },
+  { icon: Message01Icon, title: "Quotes", path: "/dashboard/quotes" },
+  {
+    icon: DiscountIcon,
+    title: "Discounts",
+    path: "/dashboard/discounts",
+    value: 10,
+  },
+];
 
-]
-
-export const navlinks :NavlinksProps[] = [
-    { icon: DashboardSquare03Icon, title: "Dashboard", path: "/dashboard" },
-    { icon: PackageIcon, title: "My Orders", path: "/dashboard/my-orders" },
-    { icon: FavouriteIcon, title: "Wishlist", path: "/dashboard/wishlist" },
-    { icon: Message01Icon, title: "Quotes", path: "/dashboard/quotes" },
-    { icon: DiscountIcon, title: "Discounts", path: "/dashboard/discounts", value: 10 },
-  ];
-
-  export const adminNavLinks: NavlinksProps[] =[
-  
-    { icon: DashboardSquare03Icon, title: "Dashboard", path: "sllers/dashboard" },
-    { icon: Message01Icon, title: "Products", path: "admin/dashboard/product-listings" },
-    { icon: PackageIcon, title: "Order list", path: "admin/dashboard/my-orders" },
-    { icon: PackageIcon, title: "Transactions", path: "admin/dashboard/my-orders" },
-    { icon: UserMultiple02Icon, title: "Customers", path: "dashboard/customers" },
-    { icon: UserAccountIcon, title: "Sellers", path: "/admin/dashboard/sellers" },
-    { icon: AlignBoxMiddleLeftIcon, title: "Applications", path: "admin/dashboard/my-orders" },
-    { icon: News01Icon, title: "Blogs", path: "admin/dashboard/discounts", value: 10 },
-    { icon: DiscountIcon, title: "Coupons/Discounts", path: "admin/dashboard/discounts", value: 10 },
-    { icon: Message01Icon, title: "Reviews", path: "admin/dashboard/Reviews" },
-    { icon: Settings02Icon, title: "Settings", path: "admin/dashboard/Reviews" },
-
-]
+export const adminNavLinks: NavlinksProps[] = [
+  { icon: DashboardSquare03Icon, title: "Dashboard", path: "sllers/dashboard" },
+  {
+    icon: Message01Icon,
+    title: "Products",
+    path: "admin/dashboard/product-listings",
+  },
+  { icon: PackageIcon, title: "Order list", path: "admin/dashboard/my-orders" },
+  {
+    icon: PackageIcon,
+    title: "Transactions",
+    path: "admin/dashboard/my-orders",
+  },
+  { icon: UserMultiple02Icon, title: "Customers", path: "dashboard/customers" },
+  { icon: UserAccountIcon, title: "Sellers", path: "/admin/dashboard/sellers" },
+  {
+    icon: AlignBoxMiddleLeftIcon,
+    title: "Applications",
+    path: "admin/dashboard/my-orders",
+  },
+  {
+    icon: News01Icon,
+    title: "Blogs",
+    path: "admin/dashboard/discounts",
+    value: 10,
+  },
+  {
+    icon: DiscountIcon,
+    title: "Coupons/Discounts",
+    path: "admin/dashboard/discounts",
+    value: 10,
+  },
+  { icon: Message01Icon, title: "Reviews", path: "admin/dashboard/Reviews" },
+  { icon: Settings02Icon, title: "Settings", path: "admin/dashboard/Reviews" },
+];
