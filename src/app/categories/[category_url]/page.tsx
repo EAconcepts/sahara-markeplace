62d67d3b146675016322cd7e2e4ba8778117ab27
Emmanuel <hahyordeji@gmail.com>
2024-09-18 19:32:44 +0100

    fixed bugs

--- src/app/categories/[category_url]/page.tsx
+++ src/app/categories/[category_url]/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
@@ -13,39 +13,49 @@ import { useGet } from "@/utils/useGet.";
 import { getUnderPrice } from "@/lib/utils";
 
 const Fashion = () => {
-  const {category_url} = useParams()
-  const [underPrice, setUnderPrice] = useState([])
-  const {data} = useGet(`category/${decodeURIComponent(category_url.toString())}`,decodeURIComponent(category_url.toString()))
-  console.log(data)
-  const categories = data?.data?.data?.categories
-  const { data:products } = useGet("cata/prod", "underPrice");
-  console.log(products)
-let productData = data?.data?.data
+  const { category_url } = useParams();
+  const [underPrice, setUnderPrice] = useState([]);
+  const { data } = useGet(
+    `category/${decodeURIComponent(category_url.toString())}`,
+    decodeURIComponent(category_url.toString()),
+  );
+  console.log(data);
+  const categories = data?.data?.data?.categories;
+  const { data: products } = useGet("cata/prod", "underPrice");
+  console.log(products);
+  // let productData = data?.data?.data;
 
-  useEffect(()=>{
-    if(decodeURIComponent(category_url.toString()) =="under_$400"){
-      getUnderPrice(products, 400,setUnderPrice)
-      productData= products?.data?.data
+  useEffect(() => {
+    if (decodeURIComponent(category_url.toString()) == "under_$400") {
+      getUnderPrice(products, 400, setUnderPrice);
+      // productData = products?.data?.data;
+      // console.log(productData);
     }
-  },[products])
+  }, [products]);
+  // console.log(productData);
+
   return (
     <div className="flex flex-col px-[24px] lg:px-[96px]">
       {/* BreadCrumb */}
       <div></div>
       <div className="flex flex-col">
-       <DeptHeader image={woman} title={decodeURIComponent( category_url.toString())} description="Discover a world of captivating and traditional fashion, from stunning wax dresses and modern loincloths to handcrafted gold and cowrie shell jewelry, rich bogolan accessories, and dazzling wedding outfits. We offer fast worldwide delivery, bringing a touch of African elegance straight to your doorstep." className=" text-blackPrimary" imageClass="lg:absolute max-lg:w-[50%] lg:left-[23%] lg:h-[254px object-cover"/>
+        <DeptHeader
+          image={woman}
+          title={decodeURIComponent(category_url.toString())}
+          description="Discover a world of captivating and traditional fashion, from stunning wax dresses and modern loincloths to handcrafted gold and cowrie shell jewelry, rich bogolan accessories, and dazzling wedding outfits. We offer fast worldwide delivery, bringing a touch of African elegance straight to your doorstep."
+          className="text-blackPrimary"
+          imageClass="lg:absolute max-lg:w-[50%] lg:left-[23%] lg:h-[254px object-cover"
+        />
         <div className="mt-[40px]">
-          <AllCategories products={productData} underPrice={underPrice}/>
+          <AllCategories products={data?.data?.data ?? products?.data?.data } underPrice={underPrice} />
         </div>
         <div className="my-[40px] py-[16px]">
-            <ProductHeader heading="Blog" showBtn={true}/>
-        <div className="grid lg:grid-cols-4 grid-cols-2 max-lg:gap-[16px] lg:gap-[24px] mt-[32px]">
-          {
-            blogs?.map((story, index)=>(
-                <BlogCard key={index} story={story}/>
-            ))
-          }
-        </div>
+          <ProductHeader heading="Blog" showBtn={true} />
+          <div className="mt-[32px] grid grid-cols-2 max-lg:gap-[16px] lg:grid-cols-4 lg:gap-[24px]">
+            {blogs?.map((story, index) => (
+              <BlogCard key={index} story={story} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
