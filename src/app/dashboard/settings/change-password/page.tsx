c27a6f5e90782f38700e56137682b7ed38825133
Emmanuel <hahyordeji@gmail.com>
2024-09-24 04:32:52 +0100

    updated vendor order dashboard

--- src/app/dashboard/settings/change-password/page.tsx
+++ src/app/dashboard/settings/change-password/page.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
-const ChangePassword = ({ url }: { url?: string }) => {
+const ChangePassword = ({ url = "" }: { url?: string }) => {
   return (
     <div className="flex flex-col gap-[32px] px-[24px] py-[28px] font-openSans">
       <div className="flex w-[80%] flex-col gap-[24px]">
