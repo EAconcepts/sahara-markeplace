cc55725def927813d73fbed43e33b91f25d63d0c
Emmanuel <hahyordeji@gmail.com>
2024-10-09 09:48:53 +0100

    protected dashboards

--- src/app/sellers/layout.tsx
+++ src/app/sellers/layout.tsx
@@ -1,12 +1,22 @@
-import { usePathname } from "next/navigation";
+"use client";
+
+import { usePathname, useRouter } from "next/navigation";
 import Topnav from "./(components)/topnav";
 import { useEffect } from "react";
+import { useAuth } from "@/utils/useAuth";
 
 export default function SellersLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const { userType, token } = useAuth();
+  const router = useRouter();
+  useEffect(() => {
+    if (userType !== "seller" && !token) {
+      router.replace("/sellers/auth/signin");
+    }
+  }, [token, userType]);
   return (
     <div>
       <Topnav />
