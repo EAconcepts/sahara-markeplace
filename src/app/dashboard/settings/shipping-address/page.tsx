3c512b65e1d3ea36248e9ba456723301ff1d648d
Emmanuel <hahyordeji@gmail.com>
2024-11-14 06:52:11 +0100

    fixed shipping address endpoint

--- src/app/dashboard/settings/shipping-address/page.tsx
+++ src/app/dashboard/settings/shipping-address/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
@@ -9,8 +11,45 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { useAuth } from "@/utils/useAuth";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { ChangeEvent, useState } from "react";
+import { toast } from "sonner";
 
 const ShippingAddress = () => {
+  const [addressDetails, setAddressDetails] = useState({
+    address: "",
+    country: "",
+    city: "",
+    state: "",
+    postal: "",
+  });
+  const { token, baseUrl } = useAuth();
+  const headers = {
+    Authorization: `Bearer ${token}`,
+  };
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setAddressDetails((prev: any) => ({ ...prev, [name]: value }));
+  };
+  const addressMutation = useMutation({
+    mutationFn: () =>
+      axios.post(`${baseUrl}/user/profile/update`, addressDetails, { headers }),
+    onSuccess: (data) => {
+      console.log(data);
+      const userData = data?.data?.data;
+      toast.success("Shipping address updated successfully!");
+    },
+    onError: (error) => {
+      console.log(error);
+      toast.error("An error occured!");
+    },
+  });
+  const handleSubmit = () => {
+    console.log(addressDetails);
+    addressMutation.mutate();
+  };
   return (
     <div className="flex w-full flex-col gap-[32px]">
       <div className="flex w-[80%] flex-col gap-[24px]">
@@ -22,7 +61,14 @@ const ShippingAddress = () => {
           >
             Address
           </label>
-          <Input id="address" type="text" className="h-[44px] w-full" />
+          <Input
+            id="address"
+            value={addressDetails.address}
+            onChange={handleChange}
+            name="address"
+            type="text"
+            className="h-[44px] w-full"
+          />
         </div>
         {/* Apartment */}
         <div className="flex w-full flex-col gap-[8px]">
@@ -44,7 +90,14 @@ const ShippingAddress = () => {
             >
               Country
             </label>
-            <Input id="country" type="text" className="h-[44px] w-full" />
+            <Input
+              id="country"
+              name="country"
+              value={addressDetails.country}
+              onChange={handleChange}
+              type="text"
+              className="h-[44px] w-full"
+            />
             {/* <Select>
               <SelectTrigger className="h-[44px] w-full">
                 <SelectValue placeholder="--Select--" />
@@ -66,7 +119,14 @@ const ShippingAddress = () => {
             >
               State
             </label>
-            <Input id="state" type="text" className="h-[44px] w-full" />
+            <Input
+              name="state"
+              value={addressDetails.state}
+              onChange={handleChange}
+              id="state"
+              type="text"
+              className="h-[44px] w-full"
+            />
           </div>
         </div>
         {/* City & Zip code */}
@@ -79,7 +139,14 @@ const ShippingAddress = () => {
             >
               City
             </label>
-            <Input id="city" type="text" className="h-[44px] w-full" />
+            <Input
+              id="city"
+              name="city"
+              value={addressDetails.city}
+              onChange={handleChange}
+              type="text"
+              className="h-[44px] w-full"
+            />
           </div>
           {/* Zip Code */}
           <div className="flex w-full flex-col gap-[8px]">
@@ -89,12 +156,22 @@ const ShippingAddress = () => {
             >
               Zip Code
             </label>
-            <Input id="zipcode" type="text" className="h-[44px] w-full" />
+            <Input
+              id="zipcode"
+              value={addressDetails.postal}
+              name="postal"
+              onChange={handleChange}
+              type="text"
+              className="h-[44px] w-full"
+            />
           </div>
         </div>
       </div>
-      <Button className="h-[36px] rounded-[8px] px-[16px] py-[8px] text-[14px] font-[600] leading-[20.3px] disabled:bg-[#E4E7EC] disabled:text-[#8E97A6]">
-        Save Changes
+      <Button
+        onClick={handleSubmit}
+        className="h-[36px] rounded-[8px] px-[16px] py-[8px] text-[14px] font-[600] leading-[20.3px] disabled:bg-[#E4E7EC] disabled:text-[#8E97A6]"
+      >
+        {addressMutation.isPending ? "Saving..." : " Save Changes"}
       </Button>
     </div>
   );
